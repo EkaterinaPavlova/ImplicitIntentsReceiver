d4b6aebfa8ffff38d9feecdbe277080fabd067af
Ekaterina Pavlova <ekaterinapavlova_96@mail.ru>
2021-11-04 21:47:12 +0300

    Last commit

--- app/src/main/java/com/hfad/implicitintentsreceiver/MainActivity.kt
+++ app/src/main/java/com/hfad/implicitintentsreceiver/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.hfad.implicitintentsreceiver
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hfad.implicitintentsreceiver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        val intent = getIntent()
+        val uri = intent.getData()
+
+        if (uri != null) {
+            val uri_string = "URI: $uri"
+            binding.textUriMessage.text = uri_string
+        }
     }
 }
